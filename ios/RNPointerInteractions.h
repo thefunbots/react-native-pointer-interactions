6dc90bbfeb2ba7dadc7045dad2595c06329c4e5d
Santi <santiripper@gmail.com>
2022-01-08 10:57:56 +0100

    fix: build with xcode 13.2

--- ios/RNPointerInteractions.h
+++ ios/RNPointerInteractions.h
@@ -1,9 +1,3 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import <React/RCTBridgeModule.h>
-#endif
 #import <React/RCTViewManager.h>
 
 @interface RNPointerInteractions : RCTViewManager
