a5f389efe97377b7d4e48130dd6f022d820d0c48
Santi <santi@thefunbots.com>
2022-03-03 01:26:58 +0100

    feat: android view fallback  (#15)
    Co-authored-by: robertherber <robert@kingstinct.com>

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 import React from "react";
+import { ViewProps } from 'react-native';
 
 export declare type PointerInteractionViewProps = {
   pointerMode?: 'automatic' | 'lift' | 'highlight' | 'hover' | 'verticalBeam' | 'horizontalBeam';
   beamLength?: number
-};
+} & ViewProps;
+
 export declare class PointerInteractionView extends React.Component<PointerInteractionViewProps>{}
---
+++ index.ios.js
@@ -0,0 +1,5 @@
+import { requireNativeComponent } from 'react-native';
+
+const PointerInteractionViewIOS = requireNativeComponent('PointerInteraction');
+
+export { PointerInteractionViewIOS as PointerInteractionView }
